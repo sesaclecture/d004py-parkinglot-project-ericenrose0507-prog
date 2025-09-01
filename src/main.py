2ad53926d1f734863e19d30bfe9d8fc09dfc3059
Micca <ericenrose0507@gmail.com>
2025-09-01 17:39:59 +0900

    과제를 제출합니다

--- src/main.py
+++ src/main.py
@@ -1,3 +1,26 @@
-"""
-Implement User Management System
-"""
+import numpy as np
+import datetime
+from parkmodule import *
+
+
+# 입차기능
+
+while True:
+
+    #입차
+    car_number = input("[입차]차량번호를 입력해주세요")
+    if car_number == 'q' :
+        break
+    incar(car_number)
+
+
+    #출차
+    out_number = input('[출차]차량번호를 입력해주세요')
+    if out_number == 'q':
+        break
+    outcar(out_number)
+
+    #종료
+    O = input("프로그램을 종료하고싶다면 'q'를 입력해주세요")
+    if O == 'q':
+        break
